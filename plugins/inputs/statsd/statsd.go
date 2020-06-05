04782e51d2fefe7bb76176282680ea08e519ab9d
Marc Ruiz <marc.ruiz@stayforlong.com>
2020-06-05 17:52:49 +0200

    Assign correct metric type to histogram metrics in statsd input

--- plugins/inputs/statsd/statsd.go
+++ plugins/inputs/statsd/statsd.go
@@ -270,7 +270,7 @@ func (s *Statsd) Gather(acc telegraf.Accumulator) error {
 			fields["start_time"] = s.lastGatherTime.Format(time.RFC3339)
 		}
 
-		acc.AddFields(m.name, fields, m.tags, now)
+		acc.AddHistogram(m.name, fields, m.tags, now)
 	}
 	if s.DeleteTimings {
 		s.timings = make(map[string]cachedtimings)
